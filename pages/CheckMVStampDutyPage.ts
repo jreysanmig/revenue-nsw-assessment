fd314cae326499b178e0226fab4bc199d370218b
jreysanmig <johnreysanmiguel@gmail.com>
2026-02-16 02:56:56 +1100

    updated page object definition to reduce duplicate declaration

--- pages/CheckMVStampDutyPage.ts
+++ pages/CheckMVStampDutyPage.ts
@@ -1,16 +1,13 @@
 import {Locator, Page} from '@playwright/test'
 
 export class CheckMVStampDutyPage {
-    page: Page
-    get checkOnlineBtn() { return this.page.locator('a', {hasText: 'Check online'})}
-    
-    constructor(page: Page) {
-        this.page = page
-    }
+    constructor(
+        readonly page: Page,
+        readonly checkOnlineBtn: Locator = page.locator('a', {hasText: 'Check online'})
+    ) {}
 
     async navigate() {
         await this.page.goto('https://www.service.nsw.gov.au/transaction/check-motor-vehicle-stamp-duty')
     }
-
     
 }
--- pages/MVRegDutyCalculatorPage.ts
+++ pages/MVRegDutyCalculatorPage.ts
@@ -1,25 +1,13 @@
 import {Locator, Page} from '@playwright/test'
 
-export class MVRegDutyCalculatorPage {
-    page: Page
-    get header() { return this.page.getByRole('heading', {name: 'Motor vehicle registration duty calculator'}) }
-    isPassengerVehicle(option: string) {
-        return this.page.locator('#passenger label', {hasText: option})
-    }
-    get purchasePrice() { return this.page.getByLabel('Purchase price or value ') }
-    get calculateBtn() { return this.page.getByRole('button', {name: 'Calculate'}) }  
-    get calculationPopup() { return this.page.locator('.modal-content', {hasText: 'Calculation'}) }
-    calculationPopupField(fieldName: string) { 
-        return this.calculationPopup.locator('tbody tr', {hasText: fieldName})
-    }
-    
-    constructor(page: Page) {
-        this.page = page
-    }
-
-    async navigate() {
-        await this.page.goto('https://www.service.nsw.gov.au/transaction/check-motor-vehicle-stamp-duty')
-    }
-
-    
+export class MVRegDutyCalculatorPage {   
+    constructor(
+        readonly page:Page,
+        readonly header: Locator = page.getByRole('heading', {name: 'Motor vehicle registration duty calculator'}),
+        readonly isPassengerVehicle = (option: string) => page.locator('#passenger label', {hasText: option}),
+        readonly purchasePrice: Locator = page.getByLabel('Purchase price or value '),
+        readonly calculateBtn: Locator = page.getByRole('button', {name: 'Calculate'}),
+        readonly calculationPopup: Locator = page.locator('.modal-content', {hasText: 'Calculation'}),
+        readonly calculationPopupField = (fieldName: string) => this.calculationPopup.locator('tbody tr', {hasText: fieldName})
+    ) {}    
 }
