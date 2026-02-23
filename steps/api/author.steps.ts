2175d2a476513e27df8dd7b5e47d40c93c59bbc6
jreysanmig <johnreysanmiguel@gmail.com>
2026-02-23 11:01:54 +1100

    Refactored step parameters to be more descriptive

--- steps/api/author.steps.ts
+++ steps/api/author.steps.ts
@@ -5,16 +5,16 @@ Given('I call the Open Library author API', async ({request, ctx}) => {
   ctx.resp = await request.get('https://openlibrary.org/authors/OL1A.json');
 });
 
-Then('the response status should be {int}', async ({ctx}, arg: number) => {
-    expect(ctx.resp.status()).toBe(arg);
+Then('the response status should be {int}', async ({ctx}, expStatusCode: number) => {
+    expect(ctx.resp.status()).toBe(expStatusCode);
 });
 
-Then('the personal_name should be {string}', async ({ctx}, arg: string) => {
+Then('the personal_name should be {string}', async ({ctx}, expPersonalName: string) => {
     const respBody = await ctx.resp.json();
-    expect(respBody.personal_name).toBe(arg);
+    expect(respBody.personal_name).toBe(expPersonalName);
 })
 
-Then('the alternate_names should include {string}', async ({ctx}, arg: string) => {
+Then('the alternate_names should include {string}', async ({ctx}, expAlternateName: string) => {
     const respBody = await ctx.resp.json();
-    expect(respBody.alternate_names).toContain(arg);
+    expect(respBody.alternate_names).toContain(expAlternateName);
 });
